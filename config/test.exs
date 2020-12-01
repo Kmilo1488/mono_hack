094a88a45f1cfcd37726f5c11dfa30d5a26e7136
Kmilo1488 <camilomoreno1488@live.com>
2020-12-01 01:40:39 -0500

    Setup CI

--- config/test.exs
+++ config/test.exs
@@ -7,11 +7,16 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :mono_hack, MonoHack.Repo,
   username: "postgres",
-  password: "postgres",
+  password: System.get_env("DB_PASSWORD") || "postgres",
   database: "mono_hack_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+  if System.get_env("GITHUB_ACTIONS") do
+   config :mono_hack, MonoHack.Repo,
+     username: "postgres",
+     password: "postgres"
+  end
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :mono_hack, MonoHackWeb.Endpoint,
